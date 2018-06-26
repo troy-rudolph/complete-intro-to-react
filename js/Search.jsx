8e00cf8bec29ea04512cdae17c064842d7f2c9ba
Troy Rudolph <troy.rudolph@alchemysystems.com>
2018-06-26 17:28:39 -0500

    e

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,14 +1,35 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 import ShowCard from './ShowCard'
 import preload from '../data.json'
 
-function Search() {
-  return <div className="search">
-    <div>
-      {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)}
-    </div>
-  </div>
-}
+class Search extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      searchTerm: 'debug debug'
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({searchTerm: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input type="text" value={this.state.searchTerm} placeholder='Search' onChange={this.handleSearchTermChange} />
+        </header>
+        <div>
+          {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
